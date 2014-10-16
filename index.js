e8438f41c5f55368687b106f4257b1342b7d66f2
ederprado <prado.eder@gmail.com>
2014-10-16 16:57:47 -0300

    Funcionando a criação das pastas

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var npm = require('npm'); // Utiliza o do package global
 var _ = require('lodash');
 require('colors');
 
+var pmTwoVersion = 'pm2@0.11.1'; // latest version 16/10/2014
+
 var args = require('yargs')
   .usage('Inicia diretório com estrutura base como "container" para um determinado projeto e instala o mesmo pelo npm'.underline.green + '\nUsage: $0 nome-pacote-npm')
 
@@ -12,7 +14,7 @@ var args = require('yargs')
   .example('$0 projeto-teste@v2.3.1', '-> projeto-teste_v2')
   .example('$0 projeto-teste@v2.3.1 -a _port_8080', '-> projeto-teste_v2_port_8080')
   .example('$0 projeto-teste@v1.1.2 -n "meu_teste_beta"', '-> meu_teste_beta')
-  .example('$0 projeto-teste@v1.2.3 --append-latest-minor', '-> projeto-teste_v1.2')
+  .example('$0 projeto-teste@v1.2.3 -m', '-> projeto-teste_v1.2')
 
   .demand(1, 'O nome do projeto que será instalado deve ser informado. Veja exemplos acima.'.red)
 
@@ -22,6 +24,7 @@ var args = require('yargs')
   .alias('name', ['n'])
   .describe('name', 'Altera o nome do diretório')
 
+  .alias('append-latest-minor', ['m'])
   .describe('append-latest-minor', 'Adiciona a minor version no final do nome do diretório')
 
   .help('h', 'Ajuda')
@@ -35,8 +38,6 @@ function log(msg) {
 log('Iniciando a criação do diretório "container" para o projeto'.green);
 
 var packageFullName = args['_'][0];
-// var name = null;
-// var version = null;
 
 npm.load({}, function (err) {
   // npm.config.set('loglevel', 'warn');
@@ -58,15 +59,15 @@ npm.load({}, function (err) {
     var minorVersion = v[2];
     var patchVersion = v[3];
 
-    // TODO: tratar parâmetros aqui que impacta no nome da pasta principal
-    // Cria diretórios baseado nos parâmetros...
-
-// .alias('append', ['a'])
-// .describe('append', 'Adiciona o texto específico no final do nome do diretório')
-// .alias('name', ['n'])
-// .describe('name', 'Altera o nome do diretório')
-// .describe('append-latest-minor', 'Adiciona a minor version no final do nome do diretório')
-    var rootPath = path.resolve('.', name + '_v' + majorVersion);
+    var rootPath = './';
+    if (args.name)
+      rootPath += args.name;
+    else if (args.append)
+      rootPath += name + '_v' + majorVersion + args.append;
+    else if (args.appendLatestMinor)
+      rootPath += name + '_v' + majorVersion + '.' + minorVersion;
+    else
+      rootPath = name + '_v' + majorVersion;
 
     log('Verifica se o diretório do projeto existe... ' + rootPath.cyan);
     if (fs.existsSync(rootPath)) {
@@ -80,35 +81,20 @@ npm.load({}, function (err) {
     fs.mkdirSync(path.resolve(rootPath, 'logs'));
     fs.mkdirSync(path.resolve(rootPath, 'node_modules'));
 
-    // npm.config.set()
-
-    npm.commands.install(rootPath, [packageFullName], function (err, data) {
+    npm.commands.install(rootPath, [packageFullName, pmTwoVersion], function (err, data) {
       if (err) {
         log('Erro ao instalar o app como dependência.'.red + '\n' + err.message);
         process.exit(1);
       }
 
-      console.log(data);
 
 
+
+
+
+
+      log( ('Pacote inicializado com sucesso no diretório: ' + rootPath).green );
 
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
   });
 });
